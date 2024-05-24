c5ac9daab37bde9dbd5b2007f648d93023b420fb
Archichil <kuhockovolec@gmail.com>
2024-05-24 13:35:55 +0300

    Initial Commit

---
+++ Currency Converter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Currency Converter
+//
+//  Created by Артур Кухоцковолец on 24.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Артур Кухоцковолец on 24.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
